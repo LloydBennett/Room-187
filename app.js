4e5454d9d62e3997b8f368afe61ae53a7a3712ee
Lloyd-Jamie Bennett <lj_bennett@hotmail.co.uk>
2023-07-28 16:53:20 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,87 @@
+require('dotenv').config()
+
+const fetch = require('node-fetch');
+const express = require('express')
+const app = express()
+const path = require('path')
+const port = 3000
+
+const Prismic = require('@prismicio/client');
+const PrismicH = require('@prismicio/helpers');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+const routes = [
+  {
+    type: 'home',
+    path: '/',
+  },
+  {
+    type: 'about',
+    path: '/our-story',
+  },
+  {
+    type: 'contact_us',
+    path: '/contact',
+  }
+]
+
+const client = Prismic.createClient(process.env.PRISMIC_ENDPOINT, {
+  accessToken: process.env.PRISMIC_ACCESS_TOKEN,
+  fetch,
+  routes,
+});
+
+//middleware
+app.use((req, res, next) => {
+  res.locals.ctx = {
+    PrismicH,
+  }
+  next()
+})
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+const handleRequest = async api => {
+  const meta = await client.getSingle('meta_details')
+  const navigation = await client.getSingle('navigation')
+  const siteDetails = await client.getSingle('site_det')
+  const members = await client.getAllByType('team_members')
+
+  return {
+    meta,
+    navigation,
+    siteDetails,
+    members
+  }
+}
+
+// Query for the root path.
+app.get('/', async (req, res) => {
+  const pageType = 'home'
+  const document = await client.getSingle('home')
+  const defaults = await handleRequest(req)
+  res.render('base', { ...defaults, document, pageType })
+})
+
+app.get('/our-story', async (req, res) => {
+  const pageType = 'about'
+  const document = await client.getSingle('about')
+  const defaults = await handleRequest(req)
+
+  res.render('base', { ...defaults, document, pageType })
+})
+
+app.get('/contact', async (req, res) => {
+  const pageType = 'contact'
+  const document = await client.getSingle('contact_us')
+  const defaults = await handleRequest(req)
+
+  res.render('base', { ...defaults, document, pageType })
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`)
+})
