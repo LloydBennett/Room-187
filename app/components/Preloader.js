3e2964c32ea08a81ae34ab9cec939e34f30548ba
Lloyd-Jamie Bennett <lj_bennett@hotmail.co.uk>
2025-01-18 22:48:10 +0000

    Preloader animation logic

--- app/components/Preloader.js
+++ app/components/Preloader.js
@@ -1,21 +1,28 @@
 import Components from 'classes/Components'
 import gsap from 'gsap'
+import { CustomEase } from "gsap/CustomEase"
 
 export default class Preloader extends Components {
   constructor() {
     super({
-      element: '[data-loader]',
       elements: {
+        loader: '[data-loader]',
         images: '[data-loader-image]',
+        imageHero: '[data-loader-hero]',
         mainTitle: '[data-main-title] [data-text-reveal]',
+        misc: '[data-misc]',
         titles: '[data-text-reveal]',
         body: 'body',
         bg: '[data-bg]',
         navBar: '[data-nav-bar]'
       }
-    })  
-    
+    })
+      
+    gsap.registerPlugin(CustomEase)
     this.tl = gsap.timeline()
+    
+    CustomEase.create("zoom", "0.71, 0, 0.06, 1")
+
     this.animate()
   }
 
@@ -27,23 +34,23 @@ export default class Preloader extends Components {
     this.elements.body.classList.add('no--scrolling')
     let delay = 2000
     let title = document.querySelector('[data-testing]')
-    // this.elements.images.forEach((img, i) => {
-    //   this.tl.to(img, { opacity: 1, duration: 0.05, ease: "linear" }, "+=0.15")
-    // });
+    
+    this.elements.images.forEach((img, i) => {
+      this.tl.to(img, { opacity: 1, duration: 0.04, ease: "linear" }, "+=0.15")
+    })
 
-    // this.tl.to(this.element, { opacity: 0, duration: 0.4, onComplete: () => {
-    //   this.element.style.display = "none"
-    // } })
-    //this.tl.fromTo(this.elements.bg, { scale: 0.3 }, { scale: 1, duration: 0.4, ease: "CubicEaseOut" })
+    this.tl.to(this.elements.imageHero, { scale: 1, width: "100%", height: "100%", duration: 0.6, ease: "zoom" }, "+=0.4")
 
-    this.tl.fromTo(this.elements.titles, { y: "100%" }, { y: 0, duration: 0.6, stagger: 0.05, onComplete: () => {
-      this.elements.body.classList.remove('no--scrolling')
+    this.tl.to(this.elements.loader, { display: "none", duration: 0.01 })
 
-    } }, "-=0.2")
-    this.tl.fromTo(this.elements.navBar, { opacity: 0}, { opacity: 1, duration: 0.4, ease: 'power2.out'})
+    this.tl.fromTo(this.elements.titles, { y: "100%" }, { y: 0, duration: 0.8, ease: "zoom", stagger: (i, target) => { return 0.05 * target.dataset.textReveal }, 
+      onComplete: () => {
+        this.elements.body.classList.remove('no--scrolling')
+      }
+    }, '-=0.1')
 
-
-    //this.tl.to(title, { opacity: 0 })
+    
+    this.tl.fromTo(this.elements.misc, { opacity: 0 }, { opacity: 1, duration: 0.6, ease: 'power2.out'}, "-=0.1")
     
   }
 }
