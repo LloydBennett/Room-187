1fc17186188e64fd2656f3bb96f86af74fd716ff
Lloyd-Jamie Bennett <lj_bennett@hotmail.co.uk>
2025-01-21 02:44:22 +0000

    Removed unneccessary js files

--- app/utils/colour.js
+++
@@ -1,8 +0,0 @@
-export const COLOUR_PASSION = '#f56308';
-export const COLOUR_GRIT_500 = '#202020';
-export const COLOUR_GRIT_400 = '#3C3C3C';
-export const COLOUR_GRIT_300 = '#535353';
-export const COLOUR_GRIT_200 = '#949494';
-export const COLOUR_GRIT_100 = '#D4D4D4';
-export const COLOUR_GRIT_50 = '#F2F2F2';
-export const COLOUR_WHITE = '#FFFFFF';
