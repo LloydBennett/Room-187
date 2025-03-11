e5e209f2560115afd1c603ed5cef10d73024dd81
Lloyd-Jamie Bennett <lj_bennett@hotmail.co.uk>
2025-03-11 02:56:31 +0000

    Resolved over flow issue in footer

--- app/classes/Page.js
+++ app/classes/Page.js
@@ -6,6 +6,9 @@ export default class Page {
     this.id = id
     this.selectors = { 
       ...elements,
+      overlay: '[data-page-transition]',
+      slideOne: '[data-page-transition-slide-one]',
+      slideTwo: '[data-page-transition-slide-two]'
     }
     Page.prototype.create = Create
     this.create()
@@ -20,24 +23,45 @@ export default class Page {
       //   autoAlpha: 1
       // })
 
-      this.animationIn.call(_ => {
-        // this.scroll = new LocomotiveScroll({
-        //   el: document.querySelector('[data-scroll-container]'),
-        //   smooth: true
-        // })
-        resolve()
-      })
+      // this.animationIn.call(_ => {
+      //   // this.scroll = new LocomotiveScroll({
+      //   //   el: document.querySelector('[data-scroll-container]'),
+      //   //   smooth: true
+      //   // })
+      //   resolve()
+      // })
+      resolve()
     })
   }
-  // hide() {
-  //   return new Promise(resolve => {
+  hide() {
+    return new Promise(resolve => {
+      this.animOut = GSAP.timeline()
+      console.log(this.selectors.overlay)
 
-  //     this.animationOut = GSAP.timeline()
+      this.animOut.to(this.selectors.overlay, {
+        clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+        duration: 0.6,
+        ease: "power2.out" 
+      })
 
-  //     this.animationOut.to(this.element, {
-  //       autoAlpha: 0,
-  //       onComplete: resolve
-  //     })
-  //   })
-  // }
+      this.animOut.to(this.selectors.slidesOne, {
+        clipPath: "polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)",
+        duration: 0.6,
+        ease: "power2.out"
+      }, '-=0.3')
+
+      this.animOut.to(this.selectors.slidesTwo, {
+        clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+        duration: 0.6,
+        ease: "power2.out"
+      }, '-=0.3').add(resolve)
+      
+      //this.animationOut = GSAP.timeline()
+
+      // this.animationOut.to(this.element, {
+      //   autoAlpha: 0,
+      //   onComplete: resolve
+      // })
+    })
+  }
 }
