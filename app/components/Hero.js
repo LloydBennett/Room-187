cbbcc0a090415f3e7b33cb381201b182b028d9d4
Lloyd-Jamie Bennett <lj_bennett@hotmail.co.uk>
2025-06-12 00:02:41 +0100

    Made tweaks to small hero fade in scroll animation

--- app/components/Hero.js
+++ app/components/Hero.js
@@ -10,7 +10,8 @@ export default class Preloader extends Components {
         heroBg: '[data-image-hero] [data-bg]'
       }
     })
-  
+
+    this.mm = gsap.matchMedia()
     this.scrollAnim()
   }
 
@@ -23,19 +24,22 @@ export default class Preloader extends Components {
     let size = this.getHeroImageSize()
     let startPos = size === "large"? '50% center': '50% 40%'
     
-    gsap.fromTo(this.elements.heroContent, 
-      { opacity: 1 },
-      {
-        opacity: 0.1,
-        scrollTrigger: {
-          trigger: this.elements.heroContent,
-          start: startPos,
-          scrub: true,
-          markers: false
-        },
-        ease: "power2.out",
-      }
-    )
+    if(size === "small") {
+      this.mm.add("(max-width: 549px)", () => {
+        this.heroContentAnim('50% center')
+      })
+    
+      this.mm.add("(min-width: 550px) and (max-width: 1199px)", () => {
+        this.heroContentAnim('80% center')
+      })
+
+      this.mm.add("(min-width: 1200px)", () => {
+        this.heroContentAnim(startPos)
+      })
+
+    } else {
+      this.heroContentAnim(startPos)
+    }
 
     gsap.fromTo(this.elements.heroBg, 
       { scale: 1 },
@@ -54,6 +58,21 @@ export default class Preloader extends Components {
   }
   getHeroImageSize() {
     return this.elements.heroImg.getAttribute('data-image-hero')
-  
+  }
+
+  heroContentAnim(pos) {
+    gsap.fromTo(this.elements.heroContent, 
+      { opacity: 1 },
+      {
+        opacity: 0.1,
+        scrollTrigger: {
+          trigger: this.elements.heroContent,
+          start: pos,
+          scrub: true,
+          markers: false
+        },
+        ease: "power2.out",
+      }
+    )
   }
 }
