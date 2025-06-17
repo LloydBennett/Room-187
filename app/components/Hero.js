7b07b180edcdfde1f9f129349622c6916dddc2e4
Lloyd-Jamie Bennett <lj_bennett@hotmail.co.uk>
2025-06-17 01:11:45 +0100

    Made tweaks to hero

--- app/components/Hero.js
+++ app/components/Hero.js
@@ -27,35 +27,25 @@ export default class Preloader extends Components {
     if(size === "small") {
       this.mm.add("(max-width: 549px)", () => {
         this.heroContentAnim('50% center')
+        this.heroImgAnim(startPos)
       })
     
       this.mm.add("(min-width: 550px) and (max-width: 1199px)", () => {
         this.heroContentAnim('80% center')
+        this.heroImgAnim('80% center')
       })
 
       this.mm.add("(min-width: 1200px)", () => {
         this.heroContentAnim(startPos)
+        this.heroImgAnim(startPos)
       })
 
     } else {
       this.heroContentAnim(startPos)
+      this.heroImgAnim(startPos)
     }
-
-    gsap.fromTo(this.elements.heroBg, 
-      { scale: 1 },
-      {
-        scale: 1.5,
-        duration: 0.6,
-        scrollTrigger: {
-          trigger: this.elements.heroContent,
-          start: startPos,
-          scrub: true,
-          markers: false
-        },
-        ease: "power2.out",
-      }
-    )
   }
+  
   getHeroImageSize() {
     return this.elements.heroImg.getAttribute('data-image-hero')
   }
@@ -65,6 +55,23 @@ export default class Preloader extends Components {
       { opacity: 1 },
       {
         opacity: 0.1,
+        scrollTrigger: {
+          trigger: this.elements.heroContent,
+          start: pos,
+          scrub: true,
+          markers: false
+        },
+        ease: "power2.out",
+      }
+    )
+  }
+
+  heroImgAnim(pos) {
+    gsap.fromTo(this.elements.heroBg, 
+      { scale: 1 },
+      {
+        scale: 1.5,
+        duration: 0.6,
         scrollTrigger: {
           trigger: this.elements.heroContent,
           start: pos,
